7e11a5e1bc1ca8a5273cdbae35b5673a01f3ed7a
TryIO <io.trydent@gmail.com>
2017-05-25 16:38:38 +0200

    one and two with comma then three

--- src/main/kotlin/io/kata/sck/Sck.kt
+++ src/main/kotlin/io/kata/sck/Sck.kt
@@ -6,5 +6,6 @@ class Sck {
   fun compute(string: String): Int =
     if (string.isBlank()) 0
     else if (string.length == 1) parseInt(string)
+    else if (string.contains(",")) string.split(",").stream().map { parseInt(it) }.reduce { v1, v2 -> v1 + v2 }.get()
     else throw RuntimeException()
 }
