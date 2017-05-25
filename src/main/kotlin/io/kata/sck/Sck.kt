c481cd5789cf7da566377051f5fbb77b331b5371
TryIO <io.trydent@gmail.com>
2017-05-25 16:52:04 +0200

    refactored method add

--- src/main/kotlin/io/kata/sck/Sck.kt
+++ src/main/kotlin/io/kata/sck/Sck.kt
@@ -3,12 +3,14 @@ package io.kata.sck
 import java.lang.Integer.parseInt
 
 class Sck {
-  fun add(string: String): Int =
-    if (string.isBlank()) 0
-    else if (string.length == 1) parseInt(string)
-    else if (string.contains(","))
-      string.split(",").stream()
-        .flatMap { it.split("\n").stream() }
-        .map { parseInt(it) }.reduce { v1, v2 -> v1 + v2 }.get()
-    else throw RuntimeException()
+  fun add(string: String): Int = when {
+    string.isBlank() -> 0
+    string.length == 1 -> parseInt(string)
+    string.contains(",") -> string.split(",").stream()
+      .flatMap { it.split("\n").stream() }
+      .map { parseInt(it) }
+      .reduce { v1, v2 -> v1 + v2}
+      .get()
+    else -> throw RuntimeException()
+  }
 }
