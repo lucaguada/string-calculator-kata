2d46516a03b16d866871cf19776b8b3d52033eea
TryIO <io.trydent@gmail.com>
2017-05-25 16:08:39 +0200

    empty string then zero test

---
+++ src/main/kotlin/io/kata/sck/Sck.kt
@@ -0,0 +1,5 @@
+package io.kata.sck
+
+class Sck {
+  fun compute(string:String):Int = if (string.isBlank()) 0 else throw RuntimeException()
+}
--- src/test/kotlin/io/kata/sck/SckTest.kt
+++ src/test/kotlin/io/kata/sck/SckTest.kt
@@ -1,11 +1,18 @@
 package io.kata.sck
 
+import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class SckTest {
+  private var sck:Sck? = null
+
+  @Before
+  fun before() { sck = Sck() }
+
   @Test
   fun emptyStringToZero() {
-    assertEquals("", 0.toString())
+    val computed = sck?.compute("")
+    assertEquals(0, computed)
   }
 }
