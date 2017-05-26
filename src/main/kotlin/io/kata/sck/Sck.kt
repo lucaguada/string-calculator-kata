1839d17e80d8f0b7d8d541a61bf955ef2e354d5b
TryIO <io.trydent@gmail.com>
2017-05-26 10:07:30 +0200

    last refactoring

--- src/main/kotlin/io/kata/sck/Sck.kt
+++ src/main/kotlin/io/kata/sck/Sck.kt
@@ -8,22 +8,26 @@ class Sck {
   fun add(string: String): Int = when {
     string.isBlank() -> 0
 
-    string.startsWith("//") -> addBy(of(string.substring(2, 3))
-      .map { string.substring(4, string.length).split(it) }
-      .get().stream())
+    string.startsWith("//") ->
+      addBy(of(string.substring(2, 3))
+        .map { string.substring(4, string.length).split(it) }
+        .get().stream())
 
-    string.length == 1 || string.contains(",") -> addBy(string.split(",").stream()
-      .flatMap { it.split("\n").stream() })
+    string.length == 1 || string.contains(",") ->
+      addBy(string.split(",").stream()
+        .flatMap { it.split("\n").stream() })
 
     else -> throw RuntimeException()
   }
 
   private fun addBy(stream: Stream<String>): Int = stream
     .map { parseInt(it) }
-    .map { when {
-      it < 0 -> throw RuntimeException("Negatives numbers not allowed: $it")
-      else -> it
-    } }
+    .map {
+      when {
+        it < 0 -> throw RuntimeException("Negatives numbers not allowed: $it")
+        else -> it
+      }
+    }
     .filter { it < 1000 }
     .reduce(0) { v1, v2 -> v1 + v2 }
 }
